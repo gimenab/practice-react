dd4402406df58dbf3674c9ada5b8969a98cc1e68
felipedcdarwoft <felipe.dallacosta@darwoft.com>
2020-10-02 16:25:58 -0300

    finishing first example of table pagination

--- react-material-kit-1.3.0/src/views/CatBreeds/TableBreeds.js
+++ react-material-kit-1.3.0/src/views/CatBreeds/TableBreeds.js
@@ -1,35 +1,41 @@
 import React, {useEffect, useState} from 'react';
-import {  Table,  TableHead, TableCell, TableBody, TableRow} from '@material-ui/core';
+import {  Table,  TableHead, TableCell, TableBody, TableRow, TablePagination} from '@material-ui/core';
+import axios from 'axios';
 
 const baseUrl= 'https://api.thecatapi.com/v1/breeds';
 
-function TableBreeds({ breeds }) {
+function TableBreeds({ breeds, handleChangePage, page }) {
     
-
-
     return (
         <div>
-                <Table>
-                    <TableHead>
+            <Table>
+                <TableHead>
+                    <TableRow>
+                        <TableCell>Nombre</TableCell>
+                        <TableCell>Origen</TableCell>
+                        <TableCell>Temperament</TableCell>
+                        <TableCell>Social Needs</TableCell>
+                    </TableRow>
+                </TableHead>
+                <TableBody>
+                    {breeds.filter((breed, index) => (index >= page * 5) && (index < page * 5 + 5)).map(breed => (
                         <TableRow>
-                            <TableCell>Nombre</TableCell>
-                            <TableCell>Origen</TableCell>
-                            <TableCell>Temperament</TableCell>
-                            <TableCell>Social Needs</TableCell>
+                            <TableCell>{breed.name}</TableCell>
+                            <TableCell>{breed.origin}</TableCell>
+                            <TableCell>{breed.temperament}</TableCell>
+                            <TableCell>{breed.social_needs}</TableCell>
                         </TableRow>
-                    </TableHead>
-                    <TableBody>
-                        {breeds.map(breed => (
-                            <TableRow>
-                                <TableCell>{breed.name}</TableCell>
-                                <TableCell>{breed.origin}</TableCell>
-                                <TableCell>{breed.temperament}</TableCell>
-                                <TableCell>{breed.social_needs}</TableCell>
-                            </TableRow>
-                        ))}
-                    </TableBody>
-                </Table>
-            
+                    ))}
+                </TableBody>
+            </Table>
+            <TablePagination
+                component="div"
+                count={100}
+                page={page}
+                rowsPerPage={5}
+                onChangePage={handleChangePage}
+                //onChangeRowsPerPage={handleChangeRowsPerPage}
+            />
         </div>
     )
 }
--- react-material-kit-1.3.0/src/views/CatBreeds/index.js
+++ react-material-kit-1.3.0/src/views/CatBreeds/index.js
@@ -2,27 +2,81 @@ import React , {useState, useEffect} from 'react';
 import axios from 'axios';
 import TableBreeds from './TableBreeds';
 
-
 function CatBreeds() {
-
 const [catBreeds, setCatBreeds] = useState([]);
-useEffect(()=> {
-    axios.get('https://api.thecatapi.com/v1/breeds')
-    .then(res => {
+const [page, setPage] = useState(0);
+
+    useEffect(()=> {
+        axios.get('https://api.thecatapi.com/v1/breeds?limit=5&page=0')
+        .then(res => {
             console.log(res);
             setCatBreeds(res.data);
-        
         })
         .catch(err => {
             console.log(err)
         })
     }, [])
-    
+
+    const handleChangePage = (event, page) => {
+        setPage(page)
+        axios.get('https://api.thecatapi.com/v1/breeds?limit=5&page=' + page)
+        .then(response => {
+            let newArray = []
+            catBreeds.forEach((element) => {
+                newArray.push(element)
+            })
+            response.data.forEach((element) => {
+                newArray.push(element)
+            })
+            setCatBreeds(newArray)
+        })
+        .catch(err => {
+            console.log(err)
+        })
+    }
+
+    // const handleChangePage = (event, page) => {
+    //     setPage(page)
+    //     axios.get('https://api.thecatapi.com/v1/breeds?limit=5&page=' + page)
+    //     .then(response => {
+    //         let newArray = Array.from(catBreeds)
+    //         response.data.forEach((element) => {
+    //             newArray.push(element)
+    //         })
+    //         setCatBreeds(newArray)
+    //     })
+    //     .catch(err => {
+    //         console.log(err)
+    //     })
+    // }
+
+    // const handleChangePage = (event, page) => {
+    //     setPage(page)
+    //     axios.get('https://api.thecatapi.com/v1/breeds?limit=5&page=' + page)
+    //     .then(response => {
+    //         let newArray = [...catBreeds, ...response.data]
+    //         setCatBreeds(newArray)
+    //     })
+    //     .catch(err => {
+    //         console.log(err)
+    //     })
+    // }
+
+    // const handleChangePage = async (event, page) => {
+    //     setPage(page)
+    //     let response = await axios.get('https://api.thecatapi.com/v1/breeds?limit=5&page=' + page)
+    //     let newArray = [...catBreeds, ...response.data]
+    //     setCatBreeds(newArray)
+    // }
+   
     return (
         <div>
             <h1>Cat Breeds</h1>
-                <TableBreeds breeds={catBreeds}></TableBreeds>
-            
+            <TableBreeds
+                breeds={catBreeds}
+                handleChangePage={handleChangePage}
+                page={page}
+            />
         </div>
     )
 }
